aed0afd92f580c730ba9589243f83fe65367b181
Julia Bouvier <juliabouv@gmail.com>
2019-08-21 15:26:35 -0700

    Wave 1: Created Order class, tests passing

---
+++ lib/order.rb
@@ -0,0 +1,27 @@
+class Order
+  
+  attr_reader :id, :products, :customer, :fulfillment_status
+  
+  def initialize(id, products, customer, fulfillment_status = :pending)
+    @id = id
+    @products = products
+    @customer = customer
+    @fulfillment_status = fulfillment_status
+    
+    valid_statuses = %i[pending paid processing shipped complete]
+    raise ArgumentError unless valid_statuses.include?(@fulfillment_status)
+  end
+  
+  def total
+    total = @products.values.reduce(0) { |sum,cost| sum + cost }
+    return (total *= 1.075).round(2)
+  end
+  
+  def add_product(product_name, price)
+    if products.has_key?(product_name)
+      raise ArgumentError
+    else
+      products[product_name] = price
+    end
+  end
+end
