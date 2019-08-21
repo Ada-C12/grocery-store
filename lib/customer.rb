fe41b98a3fa1dcd61d2419a45d983dbb73354a66
Dianna Tingg <dtingg@gmail.com>
2019-08-21 14:56:16 -0700

    added self.all and self.find and enabled part 2 tests

--- lib/customer.rb
+++ lib/customer.rb
@@ -2,9 +2,38 @@ class Customer
   attr_accessor :email, :address
   attr_reader :id
   
+  @@all = []
+  
   def initialize(id, email, address)
     @id = id
     @email = email
     @address = address
+    @@all << self
   end
+  
+  def self.all
+    return @@all
+  end
+  
+  def self.find(id)
+    customer = @@all.find do |customer|
+      customer.id == id
+    end
+    return customer    
+  end
+  
 end
+
+
+
+# one = Customer.new(1, "d@aol.com", {key: "value"})
+
+# two = Customer.new(2, "d@aol.com", {key: "value"})
+
+# puts "one.self.all"
+# p Customer.all
+
+# puts "one.self.find  should be 2"
+# p Customer.find(2)
+
+
