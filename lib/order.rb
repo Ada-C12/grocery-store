c179e224a299bd3c083d1d6cbbf0d47e9d7b7911
Kristy Hisaw <kristy.hisaw@gmail.com>
2019-08-21 14:53:05 -0700

    added Order class and initialied method

---
+++ lib/order.rb
@@ -0,0 +1,15 @@
+class Order
+  
+  attr_reader :id
+  attr_accessor :products, :customer, :fulfillment_status
+  
+  def initialize(id, products, customer, fulfillment_status)
+    
+    @id = id
+    @products = products
+    @customer = customer
+    @fulfillment_status = fulfillment_status
+    
+  end
+  
+end
