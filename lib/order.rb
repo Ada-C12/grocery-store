8d6ae550b07b31a08f88748c0823094cb6bdc67a
Kristy Hisaw <kristy.hisaw@gmail.com>
2019-08-21 15:52:04 -0700

    added total method and added valid staus check for initialize

--- lib/order.rb
+++ lib/order.rb
@@ -3,13 +3,39 @@ class Order
   attr_reader :id
   attr_accessor :products, :customer, :fulfillment_status
   
-  def initialize(id, products, customer, fulfillment_status)
+  def initialize(id, products, customer, fulfillment_status = :pending)
     
     @id = id
     @products = products
     @customer = customer
     @fulfillment_status = fulfillment_status
     
+    valid_statuses = [:pending, :paid, :processing, :shipped, :complete]
+    
+    if valid_statuses.include?(fulfillment_status)
+      return fulfillment_status
+    else
+      raise ArgumentError
+    end
+    
   end
+  
+  
+  def total
+    cost = @products.values.inject(0){ |a,b| a + b }
+    plus_tax = cost * 1.075
+    total = plus_tax.round(2)
+    
+  end
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
 end
