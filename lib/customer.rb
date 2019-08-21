ec1657a3c4bc2090708d2f9f4d43e4f5d97ec4b9
Nicky Choi <nickyjinchoi@gmail.com>
2019-08-21 15:32:00 -0700

    created self.all instance method

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require "csv"
+
 class Customer
   attr_reader :id
   attr_accessor :email, :address
@@ -6,5 +8,18 @@ class Customer
     @id = id
     @email = email
     @address = address
+  end
+
+  def self.all
+    all_customers = CSV.open("data/customers.csv", "r").map do |customer|
+      self.new(customer[0].to_i, customer[1], { street: customer[2], city: customer[3], state: customer[4], zipcode: customer[5] })
+      # city = cust[3]
+      # state = cust[4]
+      # zipcode = cust[5]
+    end
+    return all_customers
+  end
+
+  def self.find(id)
   end
 end
