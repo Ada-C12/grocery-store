d0b7197367534ed13e846a2c3f0fb53151ecbbbe
KKennedyCodes <holmstedtk@gmail.com>
2019-08-21 16:30:31 -0700

    Wave 1 - Status & Total Checks Passed, order.rb

---
+++ lib/order.rb
@@ -0,0 +1,44 @@
+require 'csv'
+
+class Order
+  
+  attr_reader :id
+  attr_accessor :products, :fulfillment_status, :customer
+  
+  def initialize(order_id, products, customer, fulfillment_status = :pending)
+    @id = order_id
+    @products = products
+    @customer = customer
+    @fulfillment_status = status_check(fulfillment_status)
+  end
+  
+  def status_check(fulfillment_status)
+    status = [:pending, :paid, :processing, :shipped, :complete]
+    if status.include?(fulfillment_status) == true 
+      return fulfillment_status
+    else
+      raise ArgumentError.new("Invalid Order Status Used")
+      return :pending
+    end
+  end
+  
+  def total
+    total = 0
+    if @products.length == 0
+      total = 0
+    else
+      @products.each do |key,value|
+        total += value
+      end
+      total = (total * 1.075).round(2)
+    end
+    return total
+  end
+  
+  # def add_product (product_name, price)
+  # end
+  
+  # def remove_product #optional
+  # end
+  
+end
