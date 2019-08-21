f50dfb744cc5a6243d0935ca33789473b0228efb
Janice Huang <janicehuang@gmail.com>
2019-08-21 16:00:30 -0700

    get to all green

--- lib/order.rb
+++ lib/order.rb
@@ -2,15 +2,18 @@ class Order
   attr_reader :id, :customer
   attr_accessor :products, :fulfillment_status
 
-  def initialize (id, products, customer)
+  def initialize (id, products, customer, fulfillment_status = :pending)
     @id = id # number
     @products = products # hash with product name as string, value as cost
     @customer = customer
-    @fulfillment_status = :pending # can be :pending, :paid, :processing, :shipped or :complete. need to raise ArgumentError if some other status is given
+    @fulfillment_status = fulfillment_status # can be :pending, :paid, :processing, :shipped or :complete. need to raise ArgumentError if some other status is given
+    okay_statuses = [:pending, :paid, :processing, :shipped, :complete]
+    raise ArgumentError, 'Invalid status.' unless okay_statuses.include?(@fulfillment_status)
   end
 
   def total
     subtotal = @products.values.reduce(:+)
+    return 0 if subtotal == nil
     total = subtotal * 1.075
     return total.round(2)
   end
@@ -19,7 +22,7 @@ class Order
     if @products.has_key?(product_name)
       raise ArgumentError, 'That product has already been added to the order.'
     else
-     @products[product_name] = price
+      @products[product_name] = price
     end
     # returns the product data to the product collection
     # if a product with the same name has been added, raise ArgumentError
