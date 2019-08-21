8b4ddbfd3a7c9cf3afd6c4d48638b4830c49fc88
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-08-21 15:02:36 -0700

    Fixed fulfillment_status :pending default

--- lib/order.rb
+++ lib/order.rb
@@ -1,14 +1,19 @@
 class Order
-
+  
   attr_reader :id
-
+  
   attr_accessor :customer, :products, :fulfillment_status
-
-  def initialize(id, products, customer, fulfillment_status)
+  
+  def initialize(id, products, customer, fulfillment_status = :pending)
     @id = id
     @products = products
     @customer = customer
-    @fulfillment_status = :pending
+    @fulfillment_status = fulfillment_status
   end
-
+  
+  def total
+    products = products.map { |item| item.values }
+    total_cost = products.sum
+  end
+  
 end
