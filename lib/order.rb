da96dabf367bae9ec9eadec16375981b70beca8f
Julia Bouvier <juliabouv@gmail.com>
2019-08-21 15:46:36 -0700

    Optional Wave 1: Implement remove_product method, tests pass

--- lib/order.rb
+++ lib/order.rb
@@ -24,4 +24,12 @@ class Order
       products[product_name] = price
     end
   end
+  
+  def remove_product(product_name)
+    if products.has_key?(product_name)
+      products.delete(product_name)
+    else
+      raise ArgumentError
+    end
+  end 
 end
