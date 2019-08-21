c8ef680e8b6af2b8857042419b97d6814438a15d
Tiffany Chio <tiffany.chio0801@gmail.com>
2019-08-21 15:20:37 -0700

    completed wave 2 for customer. passed all tests

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,11 +1,48 @@
+require 'csv'
+
 class Customer
-  # cust = Customer.new(cust_id, EMAIL, ADDRESS)
+  # cust = Customer.new(id, EMAIL, ADDRESS)
   attr_accessor :email, :address
   attr_reader :id
   
-  def initialize(cust_id, email, address)
-    @id= cust_id
+  def initialize(id, email, address)
+    @id= id
     @email = email
     @address = address
+  end
+  
+  # loads all the objects from the csv file
+  def self.all
+    all_customers = []
+    
+    # CSV file does not seem to include headers, will be read as array of arrays
+    CSV.read('data/customers.csv').each do |element|
+      id = element[0].to_i
+      email = element[1]
+      address = {street: element[2],
+        city: element[3],
+        state: element[4],
+        zip: element[5]
+      }
+      
+      # returns an array of objects
+      all_customers << Customer.new(id, email, address)
+    end
+    
+    return all_customers
+  end
+  
+  def self.find(target_id)
+    all_customers = self.all
+    matches = all_customers.select {|customer| customer.id == target_id}
+    num_matches = matches.length
+    
+    # if no customers found, returns nil like the find method for Ruby arrays
+    # returns the first match if multiple matches found, like the find method for Ruby arrays
+    if num_matches == 0
+      return nil
+    else
+      return matches[0]
+    end
   end
 end
