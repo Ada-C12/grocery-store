e8c6ac5d9456a2d73609813cf0c6459e7ae4735c
Mira <calopter@gmail.com>
2019-08-21 15:11:18 -0700

    implement Customer.all

--- lib/customer.rb
+++ lib/customer.rb
@@ -4,13 +4,15 @@ class Customer
   attr_reader :id
   attr_accessor :email, :address
 
+  CUSTOMERS_PATH = 'data/customers.csv'
+
   def initialize(id, email, address)
     @id = id
     @email = email
     @address = address
   end
 
-  def self.parse_address(street_address, city, state, zip)
+  def self.parse_address(street, city, state, zip)
     address = {}
     method(__method__).parameters.map do |_, name|
       address[name] = binding.local_variable_get(name)
@@ -19,6 +21,10 @@ class Customer
   end
 
   def self.all
-
+    all = []
+    CSV.foreach(CUSTOMERS_PATH) do |id, email, *address|
+      all << self.new(id.to_i, email, parse_address(*address))
+    end
+    return all
   end
 end
