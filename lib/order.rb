451dc1aa983736c053ce6882a0b0988fa603ba62
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-08-21 14:11:01 -0700

    added an initialize method for Order and checked to ensure fulfillment status argument is valid

--- lib/order.rb
+++ lib/order.rb
@@ -1,22 +1,20 @@
 class Order
   
+  FULFILLMENT_STATUSES = [:pending, :paid, :processing, :shipped, :complete]
+  
+  attr_reader :id, :products, :customer, :fulfillment_status
+  
+  def initialize(id, products_hash, customer, fulfillment_status = :pending)
+    @id = id
+    @products = products_hash
+    @customer = customer
+    if !FULFILLMENT_STATUSES.include?(fulfillment_status)
+      raise ArgumentError.new("#{fulfillment_status} is not a valid fulfillment status")
+    end
+    @fulfillment_status = fulfillment_status
+  end
+  
 end
-
-# Create a class called Order. Each new Order should include the following attributes:
-
-#     ID, a number (read-only)
-#     A collection of products and their cost. This data will be given as a hash that looks like this:
-
-#     { "banana" => 1.99, "cracker" => 3.00 }
-
-#         Zero products is permitted (an empty hash)
-#         You can assume that there is only one of each product
-#     An instance of Customer, the person who placed this order
-#     A fulfillment_status, a symbol, one of :pending, :paid, :processing, :shipped, or :complete
-#         If no fulfillment_status is provided, it will default to :pending
-#         If a status is given that is not one of the above, an ArgumentError should be raised
-
-# In addition, Order should have:
 
 #     A total method which will calculate the total cost of the order by:
 #         Summing up the products
