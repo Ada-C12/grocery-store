2fff01e1f9ce2d681fe44c76e73bc922d871b448
Tiffany Chio <tiffany.chio0801@gmail.com>
2019-08-21 16:52:08 -0700

    orders code passes provided wave 2 test code

--- lib/order.rb
+++ lib/order.rb
@@ -1,9 +1,12 @@
+require 'pry'
+require 'csv'
+
 class Order
   # order = Order.new(id, {}, customer, fulfillment_status)
   attr_accessor :products, :customer, :fulfillment_status
   attr_reader :id 
   
-  # do optional parameters work here
+  # do optional parameters work here - YES THEY DO
   def initialize(id, products, customer, fulfillment_status = :pending)
     if ![:pending, :paid, :processing, :shipped, :complete].include? fulfillment_status
       raise ArgumentError, 'Invalid fulfillment status.'
@@ -38,19 +41,51 @@ class Order
       raise ArgumentError, 'Product was not a part of the cart to begin with.'
     end
   end
+  
+  def self.find_fulfillment_status(a_string)
+    case a_string
+    when "pending"
+      return :pending
+    when "paid"
+      :paid
+    when "processing"
+      :processing
+    when "shipped"
+      :shipped
+    when "complete"
+      :complete
+    else
+      nil
+    end
+  end
+  
+  # "Lobster:17.18;Annatto seed:58.38;Camomile:83.21"
+  # { "banana" => 1.99, "cracker" => 3.00 }
+  def self.parse_order(a_string)
+    product_hash = {}
+    
+    a_string.split(";").each do |pair|
+      prod_price_array = pair.split(":")
+      product_hash[prod_price_array[0]] = prod_price_array[1].to_f
+    end
+    
+    return product_hash
+  end
+  
+  def self.all
+    all_orders = []
+    
+    # CSV file does not seem to include headers, will be read as array of arrays
+    CSV.read('data/orders.csv').each do |element|
+      id = element[0].to_i
+      products = Order.parse_order(element[1])
+      customer = Customer.find(element[2].to_i)
+      status = Order.find_fulfillment_status(element[3].downcase)
+      
+      # returns an array of objects
+      all_orders << Order.new(id, products, customer, status)
+    end
+    
+    return all_orders
+  end
 end
-
-# require_relative 'customer'
-
-# address = {
-#   street: "123 Main",
-#   city: "Seattle",
-#   state: "WA",
-#   zip: "98101"
-# }
-
-# cust = Customer.new(123, "a@a.co", address)
-# ord = Order.new(456, {"apple" => 4.32, "banana" => 1.99, "cracker" => 3.00}, cust)
-
-# ord.remove_product("banana")
-# puts ord.products
