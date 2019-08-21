592a6b3d4378c129610075f3205e758763d253e2
Natalie Tapias <nataliemtapias@gmail.com>
2019-08-21 16:05:38 -0700

    A few Wave 1 Initialize Tests Passed.

--- lib/order.rb
+++ lib/order.rb
@@ -2,33 +2,41 @@ require_relative 'customer'
 
 class Order
   
-  attr_accessor :collection_of_products, :fulfillment_status
-  attr_reader :customer, :id, 
+  attr_accessor :products, :fulfillment_status
+  attr_reader :id, :customer
   
+  def initialize(id, products, customer, fulfillment_status=:pending)
+    @id = id
+    @products = products
+    @customer = customer
+    @fulfillment_status = is_good_fulfillment_status?(fulfillment_status)
+    
+  end
   
-  def initialize(:id, ) 
-    @id # a number
-    @collection_of_products {}
-    @customer = Customer.new(parameters)
-    @fulfillment_status --> :pending :paid :shipped; #or if nil default is pending, or raise ArgumentError
+  def is_good_fulfillment_status?(word)
+    valid_fulfillment_status = [:pending, :paid, :processing, :shipped, :complete]
+    if !valid_fulfillment_status.include?(word) 
+      raise ArgumentError
+    else
+      return word
+    end
+    
   end
   
   def total
-    #sums products
-    # adds 7.5% tax
-    # rounds the result to two decimal places 
+    return @products.values.sum
   end
   
-  def add_product(name, price)
-    # here's a though on how to add a new item to the collection of products
-    # @collection_of_products[name] = price
-  end
+  # def add_product(name, price)
+  #   # here's a though on how to add a new item to the collection of products
+  #   # @collection_of_products[name] = price
+  # end
   
   
-  # optional (also make sure to write a test for this)
-  def remove_product(product_name)
-    # somehow delete the @collection_of_products[product_name] & its associated value from the hash
-    # if @collection_of_products[product_name] = nil, then raise ArgumentError
-  end
+  # # optional (also make sure to write a test for this)
+  # def remove_product(product_name)
+  #   # somehow delete the @collection_of_products[product_name] & its associated value from the hash
+  #   # if @collection_of_products[product_name] = nil, then raise ArgumentError
+  # end
   
 end
