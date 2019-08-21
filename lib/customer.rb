364a0540ba24d9910b5e27ba6a9ba5d692a433d2
Mira <calopter@gmail.com>
2019-08-21 14:49:30 -0700

    parse an address to has

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Customer
   attr_reader :id
   attr_accessor :email, :address
@@ -6,5 +8,17 @@ class Customer
     @id = id
     @email = email
     @address = address
+  end
+
+  def self.parse_address(street_address, city, state, zip)
+    address = {}
+    method(__method__).parameters.map do |_, name|
+      address[name] = binding.local_variable_get(name)
+    end
+    return address
+  end
+
+  def self.all
+
   end
 end
