b07627043fb6ccf36f6cfdc52e157866aa3190d8
Paige Davenport <davenport.paige@outlook.com>
2019-08-22 14:26:28 -0700

    Working self.all and self.find class methods for customer.rb

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,6 @@
+require 'csv'
+require 'awesome_print'
+
 class Customer
   attr_reader :id
   attr_accessor :email, :address
@@ -7,4 +10,22 @@ class Customer
     @email = email
     @address = address
   end
+
+  def self.all
+    customers = []
+    CSV.open('data/customers.csv', 'r+').map(&:to_a).each do |customer|
+      address = {street: customer[2], city: customer[3], state: customer[4], zip: customer[5]}
+      customers << Customer.new(customer[0].to_i, customer[1], address)
+    end
+
+    return customers
+  end
+
+  def self.find(id)
+    Customer.all.find do |customer| 
+      customer.id == id
+    end
+  end
 end
+
+
