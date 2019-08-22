38d4addcf6bd4d23c76b7127a907a424e92d10e3
C Gutierrez <gutierrez.clara.es@gmail.com>
2019-08-22 16:49:00 -0700

    Wave 2 after self.find(id) method

--- lib/customer.rb
+++ lib/customer.rb
@@ -13,28 +13,33 @@ class Customer
     @address = input_address
   end
   
-  
   # returns a collection of customer instances, representing all of the customers described in the CSV file
   def self.all
     customers = []
     CSV.open('data/customers.csv', 'r').each do |line|
-      # digests the lines of customer data from the CSV file by using their index numbers
-      imported_id = line[0].to_i
-      imported_email = line[1]
-      imported_street = line[2]
-      imported_city = line[3]
-      imported_state = line[4]
-      imported_zip = line[5]
-      # creates a hash for address (this is because for Customer class, the program is EXPECTING a hash)
-      imported_address = {:street => imported_street, :city => imported_city, :state => imported_state, :zip => imported_zip}
+      # digests the lines of customer data from the CSV file by using their index numbers and uses the information to create an instance of Customer for each line of the CSV file
       
       # creates an instance of Customer using the digested data 
-      import = Customer.new(imported_id, imported_email,imported_address)
+      import = Customer.new(line[0].to_i, line[1], {:street => line[2], :city => line[3], :state => line[4], :zip => line[5]})
       
       customers << import
     end
     return customers
   end
   
+  def self.find(id)
+    flag = nil
+    customer_collection = Customer.all
+    customer_collection.each do |customer|
+      if customer.id == id
+        flag = customer
+        break
+      else
+        flag = nil
+      end
+    end
+    return flag 
+  end
 end
+
 
