98a062b20938717cf1e70b805ebf9e83096c54d1
eaball35 <eaball35@gmail.com>
2019-08-22 15:14:30 -0700

    Finished wave 2 part 1

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,10 +1,35 @@
+require 'csv'
+require 'awesome_print'
+
 class Customer
-    def initialize(id,email_address,delivery_address)
+    def initialize(id,email,address)
         @id = id
-        @email_address = email_address
-        @delivery_address = delivery_address
+        @email = email
+        @address = address
     end
     
     attr_reader :id
-    attr_accessor :email_address, :delivery_address
+    attr_accessor :email, :address
+
+    def self.all
+        customers = []
+        CSV.open("data/customers.csv").each do |line|
+            id = line[0].to_i
+            email = line[1].to_s
+            address = {
+                street: line[2],
+                city: line[3],
+                state: line [4],
+                zip: line [5]
+            }
+            customer = Customer.new(id,email,address)
+            customers << customer
+        end
+        return customers
+    end
 end
+
+
+
+
+
