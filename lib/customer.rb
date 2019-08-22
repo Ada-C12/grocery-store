13c4d06e7939337a51be700bd8c5591764430d44
Julia Bouvier <juliabouv@gmail.com>
2019-08-22 15:01:59 -0700

    Implemented Class All method. Tests pass

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,6 @@
+require 'csv'
+require 'awesome_print'
+
 class Customer
   
   attr_reader :id
@@ -8,4 +11,21 @@ class Customer
     @email = email
     @address = address
   end
+  
+  def self.all
+    return CSV.open('../data/customers.csv', 'r+').map do |customer|
+      address = { 
+        street: customer[2],
+        city: customer[3],
+        state: customer[4],
+        zip: customer[5]
+      }
+      customer = Customer.new(customer[0].to_i, customer[1], address)
+    end
+  end
+  
+  
+  
 end
+
+
