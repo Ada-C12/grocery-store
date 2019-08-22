a5076d8d514ce3d13fdbbaa25759ff4133228266
KKennedyCodes <holmstedtk@gmail.com>
2019-08-22 14:31:07 -0700

    Wave 2 Customer Organization - customer.rb

--- lib/customer.rb
+++ lib/customer.rb
@@ -11,9 +11,16 @@ class Customer
     @address = address
   end
   
-  # def organize_customer_info(filename)
-  #   customers = csv.read('customers.csv').map(&:to_a)
-  #   binding.pry
-  #   return customers
-  # end
+  def self.all
+    customer_list = []
+    customers = CSV.read('data/customers.csv').each do |line|
+      new_customer = Customer.new(line[0].to_i, line[1].to_s, {street: line[2], city: line[3], state: line[4], zip: line[5]} )
+      # @id = line_data[0]
+      # @email = line_data[1]
+      # @address = {street: line_data[2], city: line_data[3], state: line_data[4], zip: line_data[5]}
+      customer_list.push(new_customer)
+    end
+    return customer_list
+  end
+  
 end
