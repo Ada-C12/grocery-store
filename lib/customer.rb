a529d19315e42bd03332acb2d76bf3b617ca163b
Kristy Hisaw <kristy.hisaw@gmail.com>
2019-08-22 13:59:37 -0700

    added customer.add method and opened up tests

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,8 @@
+require 'csv'
+require 'awesome_print'
+
+
+
 class Customer
   
   attr_reader :id
@@ -9,5 +14,21 @@ class Customer
     @address = address
   end
   
-  
-end 
+  def self.all
+    
+    customers = []
+    CSV.open('data/customers.csv', 'r+').map(&:to_a).each do |customer|
+      id = customer[0].to_i
+      email = customer[1]
+      address = Hash.new
+      address[:street] = customer[2]
+      address[:city] = customer[3]
+      address[:state] = customer[4]
+      address[:zip] = customer[5]
+      
+      customers << Customer.new(id, email, address)
+      
+    end
+    return customers 
+  end 
+end
