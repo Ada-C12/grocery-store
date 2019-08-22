8ab34c9fe7b1b23e216513fa90de6f6971e5c6bd
Eve Le <nguyenthao.lee@gmail.com>
2019-08-22 14:34:47 -0700

    Wave 2: Implemented class method .all for Customer class

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Customer
     attr_reader :id
     attr_accessor :email, :address
@@ -6,5 +8,15 @@ class Customer
         @id = id
         @email = email
         @address = address
+    end
+    
+    def self.all
+        output = Array.new
+        CSV.read('data/customers.csv', headers: true).each do |line| 
+            info = line.to_h
+            address = {street: info["Street_address"], city: info["City"], state: info["State"], zip: info["Zipcode"]}
+            output << Customer.new(info["ID"].to_i, info["Email"], address)
+        end
+        return output
     end
 end
