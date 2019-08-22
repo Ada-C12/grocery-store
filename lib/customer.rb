22b04a7af89165cad6a92582c4b411834d17182a
geli-gel <angele.zam@gmail.com>
2019-08-21 17:11:21 -0700

    added self.all to return all customers in csv

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Customer
   attr_reader :id
   attr_accessor :email, :address
@@ -5,5 +7,15 @@ class Customer
     @id = id
     @email = email.downcase
     @address = delivery_address
+  end
+
+  def self.all
+    customer_data = []
+    CSV.open('data/customers.csv', 'r').each do |c|
+      delivery_address = {street: c[2], city: c[3], state: c[4], zip: c[5]}
+      customer_data << Customer.new(c[0].to_i, c[1].to_s, delivery_address )
+    end
+
+    return customer_data
   end
 end
