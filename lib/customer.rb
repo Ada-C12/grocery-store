9a4164967f21f0a47263d1b31f239f7e1af1d2e6
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-08-22 15:38:04 -0700

    Passing customer self.all tests

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,5 +1,8 @@
 # customer.rb
 
+require 'csv'
+require 'awesome_print'
+
 class Customer
   # make instance variables readable from outside class
   attr_reader :id
@@ -13,6 +16,44 @@ class Customer
     @email = email
     # address is a hash
     @address = address
+  end
+  
+  def self.all
+    # initialize the array that customer objects will live in
+    customers = []
+    
+    # open csv file as an array of arrays (with each internal array representing one line of the CSV)
+    csv = CSV.open("data/customers.csv", "r")
+    
+    # iterate over each line/array of the CSV file ...
+    csv.each do |line|
+      
+      # ... pulling out id value 
+      customer_id = line[0].to_i
+      
+      # ... pulling out email value
+      customer_email = line[1]
+
+      # ... creating a hash for the address and assigning hash keys and values
+      customer_address = {}
+      customer_address[:street] = line[2]
+      customer_address[:city] = line[3]
+      customer_address[:state] = line[4]
+      customer_address[:zip] = line[5]
+
+      # ... using the values pulled from the csv data to create a new instance of the class customer
+      customer = Customer.new(customer_id, customer_email, customer_address)
+      
+      # ....and finally, pushing that newly created instance of customer to the customers array
+      customers << customer
+    end
+    
+    return customers
+  end
+  
+  
+  def self.find(id)
+
   end
   
 end
