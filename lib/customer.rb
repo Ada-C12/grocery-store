b39a91fe0659717569c0f4c0c47eeecb46863e59
dora1405 <li.dora05@gmail.com>
2019-08-22 16:23:25 -0700

    Wave 2: customer.rb self.all method complete

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,11 +1,13 @@
+require 'csv'
+require 'awesome_print'
+
+
+# Wave 1
+
 # Create a class called Customer. Each new Customer should include the following attributes:
 #   ID, a number
 #   Email address, a string
 #   Delivery address, a hash
-require 'csv'
-
-# Wave 1
-
 class Customer
   attr_reader :id
   attr_accessor :email, :address
@@ -16,5 +18,35 @@ class Customer
     
   end
   
-  
+  # Wave 2
+  # Add the following class methods to the Customer class:
+  #   self.all - returns a collection of Customer instances, representing all of the Customer described in the CSV file
+  def self.all
+    customers = []
+    data = CSV.open("data/customers.csv").map(&:to_a).each do |row|
+      address = {}
+      address[:street] = row[2]
+      address[:city] = row[3]
+      address[:state] = row[4]
+      address[:zip] = row[5]
+      customer = Customer.new(row[0].to_i, row[1], address)
+      customers << customer
+      
+    end
+    
+    return customers
+  end
 end
+
+
+
+# self.find(id) - returns an instance of Customer where the value of the id field in the CSV matches the passed parameter
+# def self.find(id)
+
+
+
+# end
+
+
+
+
