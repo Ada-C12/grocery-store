56a9c7e7412d253ebb8fd59cc70ecfaad5d14c9a
Amal Hassan-Ali <amalh.a97@gmail.com>
2019-08-22 15:52:53 -0700

    finished self.all method

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,18 +1,58 @@
+require 'csv'
+require 'awesome_print'
 class Customer 
-
-attr_reader :id 
-attr_accessor :email, :address
-
+    
+    attr_reader :id 
+    attr_accessor :email, :address
+    
     def initialize(id,email,address)
         @id = id
         @email = email
         @address = address
+        
+        
+        
+        
+        
+        
+        
+    end 
+    def self.all
+        data = CSV.read('data/customers.csv')
+        customers = []
+        data.each do |person|
 
-
-
-
-
+            address = {street: person[2], city: person[3], state: person[4], zip: person[5]}
+            csv_instance = self.new(person[0].to_i, person[1], address)
+            customers << csv_instance 
+            
+            
+        end 
+        customers
+        
+        
+        
+        
+        
+        
+        
+        
     end 
 
+  
 
+
+
+    
+    
+    
+    
 end 
+
+
+
+
+
+
+
+
