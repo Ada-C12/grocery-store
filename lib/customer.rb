8361b22f07216a2e82733bf61ce3058c71b3dd23
Nicky Choi <nickyjinchoi@gmail.com>
2019-08-22 14:25:36 -0700

    added lines of code to self.all method

--- lib/customer.rb
+++ lib/customer.rb
@@ -19,15 +19,9 @@ class Customer
       row
     end
 
+    # loop through each customer's info to obtain id, email, and address
     customers.each do |cust|
-      # self.new(customer[0].to_i, customer[1], { street: customer[2], city: customer[3], state: customer[4], zipcode: customer[5] })
-      id = cust[0].to_i
-      email = cust[1]
-      address = { street: cust[2], city: cust[3], state: cust[4], zip: cust[5] }
-      all_customers << self.new(id, email, address)
-      # city = cust[3]
-      # state = cust[4]
-      # zipcode = cust[5]
+      all_customers << self.new(cust[0].to_i, cust[1], { street: cust[2], city: cust[3], state: cust[4], zip: cust[5] })
     end
     return all_customers
   end
