868cef9c49a54d50035723b22d1c2ba128ed499f
Janice Huang <janicehuang@gmail.com>
2019-08-21 17:18:19 -0700

    initiate wave 2 in tests files; make self.all in customer.rb

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,6 +1,24 @@
 class Customer
+  require 'csv'
   attr_reader :id
   attr_accessor :email, :address
+
+  def self.all
+    customers_relevant = []
+    all_customers = CSV.read('../data/customers.csv').map(&:to_a)
+    all_customers.each do |cur_customer|
+      id = cur_customer[0]
+      email = cur_customer[1]
+      address = cur_customer[2..5]
+      customer = Customer.new(id, email, address)
+      customers_relevant << customer
+    end
+  return customers_relevant
+  end
+
+  def self.find(id)
+    self.all.select {|customer| customer.id == id}
+  end
 
   def initialize (id, email, address)
     @id = id # number
--- test/order_test.rb
+++ test/order_test.rb
@@ -113,8 +113,8 @@ describe "Order Wave 1" do
   end
 end
 
-# TODO: change 'xdescribe' to 'describe' to run these tests
-xdescribe "Order Wave 2" do
+
+describe "Order Wave 2" do
   describe "Order.all" do
     it "Returns an array of all orders" do
       # TODO: Your test code here!
