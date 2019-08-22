5eaff4ad2bc9537d722d78acd84ff12c0c3cdcf8
Vi <vanessareigosa@gmail.com>
2019-08-21 20:37:57 -0700

    wave 1 all tests passed!

--- lib/order.rb
+++ lib/order.rb
@@ -1,23 +1,47 @@
 class Order
   attr_reader :id 
+  attr_accessor :products, :customer, :fulfillment_status
 
-  def initialize(id, products, customer, fulfillment_status)
+  def initialize(id, products, customer, fulfillment_status = :pending)
     @id = id 
     @products = products
     @customer = customer
     @fulfillment_status = fulfillment_status
+    shipping = [:pending, :paid, :processing, :shipped, :complete]
+      
+    if !shipping.include? @fulfillment_status 
+      raise ArgumentError
+    end
+    #create an array of fulfillment status, check to see if included, if not, raise argument error
   end
 
-  # def total
-  #   products.each do |product, cost|
-  #   end
-  # # calculate the total cost of the order by:
-  # # Summing up the products
-  # # Adding a 7.5% tax
-  # # Rounding the result to two decimal places
-  # end
+  def total
+    
+    prices = @products.values 
+    price_without_tax = prices.sum 
+    tax = price_without_tax * 0.075
+    total_sum = tax + price_without_tax 
+    total_sum.round(2)
+    #Initialize variable outside of loop 
+    #Iterate through each key(product) + value(price) in the products hash
+    #
 
-  # def add_product(product, price)
+
+    # calculate the total cost of the order by:
+    # Summing up the products
+    # Adding a 7.5% tax
+    # Rounding the result to two decimal places
+  end
+
+  def add_product(name, price)
+
+    if @products.key?(name)
+      raise ArgumentError
+    end
+
+    @products[name] = price
+  end
+
   #   # if variable.callmethod() != nil 
   #   #   #Raise Argument Error here
   #   # else 
