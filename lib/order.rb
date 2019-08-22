db2144f032e19b4a30718d0868b41cd5ff8203fd
Nicky Choi <nickyjinchoi@gmail.com>
2019-08-21 20:24:13 -0700

    changed customer id to an instance of Customer

--- lib/order.rb
+++ lib/order.rb
@@ -1,6 +1,9 @@
+require "pry"
+require "csv"
+
 class Order
   attr_reader :id
-  attr_accessor :customer, :products, :fulfillment_status
+  attr_accessor :products, :customer, :fulfillment_status
 
   def initialize(id, products, customer, fulfillment_status = :pending)
     @id = id
@@ -17,7 +20,7 @@ class Order
   def total
     total_products_cost = @products.values.sum
     total_sum = total_products_cost * 1.075
-    puts(total_sum.round(2))
+    # puts(total_sum.round(2))
     return total_sum.round(2)
   end
 
@@ -27,7 +30,31 @@ class Order
     else
       @products[product_name] = price
     end
-    puts("productsproducts")
-    print(@products.inspect)
+    # puts("productsproducts")
+    # print(@products.inspect)
+  end
+
+  def self.list_of_products_hash(products_string)
+    products = {}
+    products_string.split(";").each do |prod|
+      prod = prod.split(":")
+      key = prod[0]
+      value = prod[1]
+      products[key] = value.to_f
+    end
+    return products
+  end
+
+  def self.all
+    all_the_orders = CSV.open("data/orders.csv", "r").map do |order_info|
+      self.new(order_info[0].to_i, list_of_products_hash(order_info[1]), Customer.find(order_info[2].to_i), order_info[3].to_sym)
+    end
+
+    return all_the_orders
+  end
+
+  def self.find(id)
+    orders = self.all
+    return order.find { |order| order.id == id }
   end
 end
