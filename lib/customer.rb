2f9d6e56f9fff419836a93db2ca9f36300403e66
Morgan Schuler <morganlschuler@gmail.com>
2019-08-23 11:09:59 -0700

    First part of wave 2 completed for customer.rb

--- lib/customer.rb
+++ lib/customer.rb
@@ -7,4 +7,29 @@ class Customer
     @email = email
     @address = address
   end
-end
+  
+  def self.all
+    @customers = []
+    CSV.open("data/customers.csv", "r").each do |line|
+      id = line[0]
+      email = line[1]
+      address = {"street-address" => line[2], 
+        "city" => line[3], 
+        "state" => line[4], 
+        "zip-code" => line[5]}
+        @customers << Customer.new(id.to_i, email, address)
+      end
+      return @customers
+      
+    end
+    
+    def self. find(id)
+      customers = Customer.all
+      return customer.find do |customer|
+        customer.id == id.to_i
+      end
+    end
+    
+    
+    
+  end
