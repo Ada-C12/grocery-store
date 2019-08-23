78b6e77a75d3f34d01f37da3083d0c4a12497bd0
Natalie Tapias <nataliemtapias@gmail.com>
2019-08-22 22:36:51 -0700

    Wave 2 Customer.all -- tests pass.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,11 +1,26 @@
+
+require 'csv'
+require 'pry'
+require 'awesome_print'
+
+
 class Customer
-  
   attr_reader :id
   attr_accessor :email, :address
   
   def initialize(id, email, address)
-    @id = id
+    @id = id 
     @email = email
     @address = address
   end
+  
+  
+  def self.all 
+    array = CSV.read('data/customers.csv').map do |row|
+      Customer.new(row[0].to_i, row[1], {street: row[2], city: row[3], state: row[4], zip: row[5]})
+    end
+    # binding.pry
+    return array
+  end
+  
 end
