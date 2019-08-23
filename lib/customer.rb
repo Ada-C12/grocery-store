d2343d36e13ab4c668ba7a89b92e38529bc1c48f
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2019-08-22 19:44:54 -0700

    Wrote the self.all method for customer. Passes tests.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,5 +1,8 @@
 class Customer
   
+  require "csv"
+  
+  
   attr_reader :id
   attr_accessor :email, :address
   
@@ -8,4 +11,24 @@ class Customer
     @email = email
     @address = address
   end 
+  
+  def self.all  
+    
+    customers = CSV.read('data/customers.csv')
+    new_cust_array = []
+    customers.each do |array|
+      id = array[0].to_i
+      email = array[1]
+      address = {:street => "#{array[2]}", :city => "#{array[3]}", :state => "#{array[4]}", :zip => "#{array[5]}"}
+      new_cust_array << Customer.new(id, email, address)
+    end
+    return new_cust_array
+  end
+  
+  
+  def self.find(id)
+  end
+  
 end
+
+
