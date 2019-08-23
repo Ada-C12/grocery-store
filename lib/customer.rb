e62e57b72888b1b2cd9dc3c4a4c902ceb6c8cb3f
Raisah Vesteinsdottir <rvesteinsdottir@gmail.com>
2019-08-23 14:13:09 -0700

    Refactored customer.rb and order.rb

--- lib/customer.rb
+++ lib/customer.rb
@@ -4,33 +4,40 @@ class Customer
   attr_reader :id
   attr_accessor :email, :address
   
-  def initialize (id, email_address, delivery_address)
+  def initialize(id, email_address, delivery_address)
     @id = id
     @email= email_address
     @address = delivery_address
   end
   
+  # returns collection of customers represented in CSV file
   def self.all
     file = CSV.read('data/customers.csv').map(&:to_a)
     
     all_customers  = file.map do |new_cust|
-      Customer.new(new_cust[0].to_i, new_cust[1], {street: new_cust[2], city: new_cust[3], state: new_cust[4], zip: new_cust[5]})
+      address = { street: new_cust[2], city: new_cust[3], state: new_cust[4], zip: new_cust[5] }
+      
+      Customer.new(new_cust[0].to_i, new_cust[1], address)
     end
     
     return all_customers
   end
   
+  # finds customer based on customer id
   def self.find(id)
-    customers = Customer.all
+    all_customers = Customer.all
     
-    return customers.find {|customer| customer.id == id}
+    selected_customer = all_customers.find { |customer| customer.id == id }
+    
+    return selected_customer
   end
   
+  # saves customers in same format as original CSV
   def self.save(filename)
-    customers = Customer.all
+    all_customers = Customer.all
     
     CSV.open(filename, "w") do |file|
-      customers.each do |customer|
+      all_customers.each do |customer|
         file << [customer.id, customer.email, customer.address[:street], customer.address[:city], customer.address[:state], customer.address[:zip]]
       end
     end
--- lib/order.rb
+++ lib/order.rb
@@ -1,5 +1,4 @@
 require 'csv'
-require_relative 'customer'
 
 class Order
   VALID_STATUS = [:pending, :paid, :processing, :shipped, :complete]
@@ -13,89 +12,94 @@ class Order
     @customer = customer
     @fulfillment_status = fulfillment_status
     
-    unless VALID_STATUS.include?(@fulfillment_status)
-      raise ArgumentError, "Fulfillment status not valid"
-    end
+    raise ArgumentError, "Fulfillment status not valid" unless VALID_STATUS.include?(@fulfillment_status)
   end
   
   def total
-    if @products.length == 0
-      return 0
-    end
+    return 0 if @products.length == 0
+    
     cost = @products.values.reduce(:+)
-    cost += (cost * 0.075)
+    cost *= 1.075
     return cost.round(2)
   end
   
+  # adds product to order
   def add_product(product_name, product_price)
-    if @products.keys.include?(product_name)
-      raise ArgumentError, "Product already exists in Grocery Store"
-    end
+    raise ArgumentError, "Product already exists in order" if @products.keys.include?(product_name)
+    
     @products["#{product_name}"] = product_price
   end
   
+  # removes product from order
   def remove_product(product_name)
-    unless @products.keys.include?(product_name)
-      raise ArgumentError, "Product does not  exist in Grocery Store"
-    end
+    raise ArgumentError, "Product does not exist in order" unless @products.keys.include?(product_name)
+    
     @products.delete(product_name)
   end
   
+  # returns collection of orders represented in CSV file
   def self.all
     file = CSV.read('data/orders.csv').map(&:to_a)
     
-    all_orders  = file.map do |new_cust|      
+    all_orders = file.map do |new_cust|      
       Order.new(new_cust[0].to_i, products_to_hash(new_cust[1]), Customer.find(new_cust[2].to_i), new_cust[3].to_sym)
     end
-  end
-  
-  def self.products_to_hash(products_as_string)
-    products_hash = {}
-    products_array = products_as_string.split(';')
-    products_array.each do |product|
-      product_array = product.split(':')
-      products_hash[product_array[0]] = product_array[1].to_f
-    end
-    return products_hash
-  end
-  
-  def self.find(id)
-    orders = Order.all
     
-    return orders.find {|order| order.id == id}
+    return all_orders
   end
   
+  # finds order based on order id
+  def self.find(id)
+    all_orders = Order.all
+    
+    selected_order = all_orders.find { |order| order.id == id }
+    
+    return selected_order
+  end
+  
+  #returns list of order instance with specified customer id
   def self.find_by_customer(customer_id)
-    orders = Order.all
-    orders_by_customer = orders.find_all {|order| order.customer.id == customer_id}
+    all_orders = Order.all
+    orders_by_customer = all_orders.find_all { |order| order.customer.id == customer_id }
     
     return nil if orders_by_customer.length == 0
     
     return orders_by_customer
   end
   
+  # converts products string to hash
+  def self.products_to_hash(products_as_string)
+    products_hash = {}
+    products_array = products_as_string.split(';')
+    
+    products_array.each do |product|
+      product_array = product.split(':')
+      products_hash[product_array[0]] = product_array[1].to_f
+    end
+    
+    return products_hash
+  end
+  
+  # saves orders in same format as original CSV
   def self.save(filename)
-    orders = Order.all
+    all_orders = Order.all
     
     CSV.open(filename, "w") do |file|
-      orders.each do |order|
+      all_orders.each do |order|
         file << [order.id, products_to_string(order.products), order.customer.id, order.fulfillment_status.to_s]
       end
     end
   end
   
+  # converts products hash to string
   def self.products_to_string(products_hash)
     products_string = ""
-    index = 1
-    products_hash.each do |name, cost|
+    
+    products_hash.each_with_index do |(name, cost), index|
       products_string << "#{name}:#{cost}"
-      unless index == products_hash.length
-        products_string << ";"
-      end
-      index += 1
+      products_string << ";" unless (index + 1) == products_hash.length
     end
     
     return products_string
   end
-  
 end
