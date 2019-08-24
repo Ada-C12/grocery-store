24e4ba500e88897440be3c461e0e04d414402210
dora1405 <li.dora05@gmail.com>
2019-08-23 20:06:38 -0700

    Wave 2: order.rb completed self.all method

--- lib/order.rb
+++ lib/order.rb
@@ -1,6 +1,5 @@
 require 'csv'
 
-
 # Create a class called Order. Each new Order should include the following attributes:
 #   ID, a number (read-only)
 #   A collection of products and their cost
@@ -44,14 +43,52 @@ class Order
     if @products.key?(product_name)
       raise ArgumentError.new "Product of the same name had already been added to this order."
     end
-
+    
     @products[product_name] = price
     
   end
   
+  def self.all
+    orders = []
+    data = CSV.open("data/orders.csv").map(&:to_a).each do |row|
+      product_array = []
+      product_split = row[1].split(';')
+      product_split.each do |product_string|
+        product_array << product_string.split(':')
+      end
+      product_hash = Hash[product_array]
+      products = {}
+      product_hash.each do |item, price|
+        price = price.to_f
+        products[item] = price
+      end
+      
+      order = Order.new(row[0].to_i, products, Customer.find(row[2].to_i), row[3].to_sym)
+      orders << order
+      
+      
+    end
+    return orders
+    
+  end
+  
+  
+  def self.find(id)
+    data = Order.all
+    data.find do |order|
+      if order.id == id
+        order_info = order
+      end
+    end
+    
+    return order_info
+    
+    
+  end
+  
+  
+  
 end
-
-
 
 # ap CSV.open(filename, headers: true)
 # ap CSV.read(filename)
