d722ff50f26e22cd9b8463dc195c7601192a01df
Julia A Kingrey <juliakingreyworks@gmail.com>
2019-08-23 17:02:00 -0700

    All wave 2 tests for order.all (including the two I wrote) are passing.

--- lib/customer.rb
+++ lib/customer.rb
@@ -2,6 +2,7 @@
 
 require 'csv'
 require 'awesome_print'
+require 'pry'
 
 class Customer
   # make instance variables readable from outside class
@@ -28,32 +29,42 @@ class Customer
     # iterate over each line/array of the CSV file ...
     csv.each do |line|
       
-      # ... pulling out id value 
+      # ... pull out id value and convert it to an integer 
       customer_id = line[0].to_i
       
-      # ... pulling out email value
+      # ... pull out email value
       customer_email = line[1]
-
-      # ... creating a hash for the address and assigning hash keys and values
+      
+      # ... create a hash for the address and assigning hash keys and values
       customer_address = {}
       customer_address[:street] = line[2]
       customer_address[:city] = line[3]
       customer_address[:state] = line[4]
       customer_address[:zip] = line[5]
-
-      # ... using the values pulled from the csv data to create a new instance of the class customer
+      
+      # ... use the values pulled from the csv data to create a new instance of the class customer
       customer = Customer.new(customer_id, customer_email, customer_address)
       
-      # ....and finally, pushing that newly created instance of customer to the customers array
+      # ....and finally, push that newly created instance of customer to the customers array
       customers << customer
     end
     
     return customers
+    
   end
-  
   
   def self.find(id)
-
+    # Invoke Customer.all to produce our formatted array of customer objects
+    customers = Customer.all
+    
+    # Iterate over each customer in the array ...
+    customers.each do |customer|
+      # ... determine if id in the object matches the id in the method argument
+      if customer.id == id
+        # ... and if the id matches, return the customer object
+        return customer
+          end
+    end
+    return nil
   end
-  
 end
--- lib/order.rb
+++ lib/order.rb
@@ -1,5 +1,7 @@
 # lib/order.rb
 
+require 'pry'
+
 class Order
   # make instance variables readable outside class
   attr_reader :id, :products, :customer, :fulfillment_status
@@ -12,10 +14,8 @@ class Order
     @products = order_items_and_prices
     # customer is an instance of Customer class
     @customer = customer
-    
     # default value for fulfillment_status is :pending
-    @fulfillment_status = fulfillment_status
-    
+    @fulfillment_status = fulfillment_status 
     # validate fulfillment_status for acceptable statuses
     valid_fulfillment_statuses = [:pending, :paid, :processing, :shipped, :complete]
     if !valid_fulfillment_statuses.include?(fulfillment_status)
@@ -53,6 +53,66 @@ class Order
       products[product_name] = price
     end
   end 
+  
+  def self.all
+    # initialize the array that order objects will live in 
+    orders = []
+    
+    # open the csv file as an array of arrays (one csv line = one array)
+    
+    csv = CSV.open("data/orders.csv", "r")
+    
+    # iterate over each line/array of the CSV file and ...
+    csv.each do |line|
+      
+      # ... pull out the order_id value and convert it to an integer
+      order_id = line[0].to_i
+      
+      # ... pull out all the product details from csv and put them 
+      # in one big long string
+      big_long_products_string = line[1..-3].join(" ")
+      
+      # ... divide up the big long string by semicolon
+      split_by_semicolons = big_long_products_string.split(';')
+      
+      # ... divide strings created above into strings split by
+      # colons and put these even-smaller strings into an array
+      split_by_colons = []
+      
+      split_by_semicolons.each do |string|
+        split_by_colons << string.split(':')
+      end
+      
+      # ... now we have an array where even-index elements are products and 
+      # odd-index elements are prices that we can transform into a hash
+      products = {}
+      products = split_by_colons.to_h
+      
+      # ... convert values in products hash from string to float
+      products.transform_values!(&:to_f)
+      
+      # ... assign customer_id variable's value
+      customer_id = line[-2].to_i      
+      
+      # ... use Customer.find class method to look up customer object 
+      # by customer ID
+      customer = Customer.find(customer_id)
+      
+      # ... assign fulfillment_status variable's value and convert it to a symbol
+      fulfillment_status = line[-1].to_sym
+      
+      # ... use the values pulled from the csv data to create a new instance of the
+      # class order
+      order = Order.new(order_id, products, customer, fulfillment_status)
+      
+      # ... and lastly, push that newly created order into the orders array
+      orders << order
+      
+    end
+    
+    return orders
+    
+  end
   
 end
 
