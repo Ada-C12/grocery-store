7ea30d0f72c637522f9fbf416f0bc9ad8eac61c7
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-08-25 12:56:19 -0700

    Fixed formatting & made minor changes

--- lib/customer.rb
+++ lib/customer.rb
@@ -14,17 +14,29 @@ class Customer
   def self.all
     customers = CSV.open('data/customers.csv', 'r+').map do |cust|
       address = { street: cust[2], city: cust[3],state: cust[4],zip: cust[5]}
-      cust = Customer.new(cust[0].to_i, cust[1], address)
+      Customer.new(cust[0].to_i, cust[1], address)
     end
     return customers
   end
   
   def self.find(id)
     customers = self.all
-    customers.find do |customer|
-      customer.id == id
+    customers.find do |cust|
+      cust.id == id
     end
   end
   
+  def self.save(file)
+    File.open(file, 'a+') do |content|
+      self.all.each do |cust|
+        address_string = ""
+        # Retrieves address line from address hash and creates a comma-separated string
+        cust.address.each do |key, value|
+          address_string += "#{value},"
+        end
+        content << "#{cust.id},#{cust.email},#{address_string.delete_suffix(',')}\n"
+      end
+    end
+  end
   
 end
--- lib/order.rb
+++ lib/order.rb
@@ -1,7 +1,6 @@
 class Order
   
-  attr_reader :id
-  
+  attr_reader :id  
   attr_accessor :customer, :products, :fulfillment_status
   
   def initialize(id, products, customer, fulfillment_status = :pending)
@@ -55,7 +54,7 @@ class Order
       
       customer = Customer.find(cust.to_i)
       
-      order = Order.new(id.to_i, product_hash, customer, fulfillment_status.to_sym)
+      Order.new(id.to_i, product_hash, customer, fulfillment_status.to_sym)
     end
     return orders
   end
@@ -69,7 +68,6 @@ class Order
   
   def self.find_by_customer(customer_id)
     orders = self.all
-    customers = Customer.all
     orders_by_customer = []
     orders.map do |order|
       if order.customer.id == customer_id
@@ -90,8 +88,7 @@ class Order
         products.each do |product|
           product_string += "#{product};"
         end
-        order = "#{order.id},#{product_string.delete_suffix(';')},#{order.customer.id},#{order.fulfillment_status}\n"
-        content << order
+        content << "#{order.id},#{product_string.delete_suffix(';')},#{order.customer.id},#{order.fulfillment_status}\n"
       end
     end
   end
