6e5a888640b03668978f91d89c952b0b2f885d05
brilatimer <brihiramine@gmail.com>
2019-08-24 18:35:36 -0700

    began wave 2: created method for self.all for Order, created helper method to parse

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,7 @@
+# Wave 1
+require 'csv'
+require 'pry'
+
 # customer class
 class Customer 
   attr_reader :id # a number
@@ -7,5 +11,23 @@ class Customer
     @id = input_id
     @email = input_email_address
     @address = input_delivery_address
+  end
+  
+  # Wave 2
+  # create method self.all for Customer
+  def self.all # .all returns an array of Customer instances
+    customer_data = []
+    CSV.read("/Users/bri/Documents/ada-hw/week-three/grocery-store/data/customers.csv").each do |customer|
+      id, email, street, city, state, zip = customer
+      customer_data.push Customer.new(id.to_i, email, {street: street, city: city, state: state, zip: zip})
+    end
+    return customer_data
+  end
+  
+  # create method self.find(id) for Customer
+  def self.find(id)
+    customers = Customer.all
+    customer = customers.find {|customer_instance| customer_instance.id == id}
+    return customer
   end
 end
--- lib/order.rb
+++ lib/order.rb
@@ -1,3 +1,7 @@
+require 'csv'
+require 'pry'
+require_relative 'customer'
+
 # create order class
 class Order 
   attr_reader :id, :fulfillment_status, :customer
@@ -7,11 +11,11 @@ class Order
   
   def initialize(input_id, input_products, input_customer, input_fulfillment_status = :pending)
     if !STATUS.include?(input_fulfillment_status) 
-      raise ArgumentError
+      raise ArgumentError 
     end
     
     @id = input_id
-    @products = input_products
+    @products = input_products # hash
     @zero_products = {}
     @customer = input_customer
     @fulfillment_status = input_fulfillment_status
@@ -20,7 +24,7 @@ class Order
   # create method 'total'. Sum products, add 7.5% sales tax to each product, round result to 2 decimal places
   def total
     total = 0
-    total = @products.each do |key, value|
+    @products.each do |key, value|
       total += value
     end
     grand_total = (total * 1.075).round(2)
@@ -29,9 +33,37 @@ class Order
   
   # create method for 'add_product
   def add_product(product_name, price)
-    add_product = Order.new(@name, @price)
-    if add_product.contains add_product
+    if @products.key? product_name
       raise ArgumentError.new ("Duplicate product exists")
     end
+    @products[product_name] = price
   end
+  
+  # create helper method to transform order string to hash
+  def self.parse(cart)
+    cart_contents = {}
+    cart.split(";").each do |item_pair|
+      item, price = item_pair.split(":")
+      cart_contents[item] = price.to_f
+    end
+    return cart_contents
+  end
+  
+  # create method self.all for Order
+  def self.all # .all returns an array of Order instances
+    order_data = []
+    CSV.read("/Users/bri/Documents/ada-hw/week-three/grocery-store/data/orders.csv").each do |order|
+      order_id, products, customer_id, status = order
+      order_data.push Order.new(order_id.to_i, self.parse(products), Customer.find(customer_id.to_i), status.to_sym)
+    end
+    return order_data
+  end
+  
+  # create method self.find(id) for Order
+  #  def self.find(id)
+  #   customers = Customer.all
+  #   customer = customers.find {|customer_instance| customer_instance.id == id}
+  #   return customer
+  # end
 end
+
