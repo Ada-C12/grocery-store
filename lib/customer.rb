104f4a468e8b6d62f6b592f1c75f71b473020ba8
Vi <vanessareigosa@gmail.com>
2019-08-26 10:11:29 -0700

    all tests passed!

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,11 +1,54 @@
+require 'csv'
+require 'pry'
+
 class Customer
   attr_reader :id
   attr_accessor :email, :address
-
+  
   def initialize(id, email, address)
     @id = id
     @email = email
     @address = address
   end
   
+  # returns a collection of `Customer` instances, representing all of the Customer described in the CSV file
+  
+  #Iterating through the allcustomers array per customer, or element in that array
+  #Taking each customer and extracting id at index 0 
+  
+  #I want to iterate over each customers(an arrray) addresses(also an array)
+  #Create a hash
+  #I want to assign keys (labels) to the values (data) for their addresses
+
+  #Each customer object here will be an instance of that customers array 
+  def self.all
+    allcustomers = CSV.read('data/customers.csv').map(&:to_a)
+    customers = []
+
+    allcustomers.each do |customer_data|
+      customers << Customer.new(customer_data[0].to_i, customer_data[1], customer_data[2..5])
+    end
+
+    customers.each do |customer|
+      address = {}
+      address[:street] = customer.address[0] 
+      address[:city] = customer.address[1]
+      address[:state] = customer.address[2]
+      address[:zip] = customer.address[3]
+
+      customer.address = address 
+    end
+    return customers
+  end
+    
+  # - `self.find(id)` - returns an instance of `Customer` where the value of the id field in the CSV matches the passed parameter
+  def self.find(id)
+    customers = Customer.all
+  
+    found = customers.find do |customer|
+      customer.id == id 
+    end
+    return found
+  end 
+
 end 
--- lib/order.rb
+++ lib/order.rb
@@ -12,21 +12,16 @@ class Order
     if !shipping.include? @fulfillment_status 
       raise ArgumentError
     end
-    #create an array of fulfillment status, check to see if included, if not, raise argument error
   end
 
   def total
-    
     prices = @products.values 
-    price_without_tax = prices.sum 
-    tax = price_without_tax * 0.075
-    total_sum = tax + price_without_tax 
+    total_without_tax = prices.sum 
+    tax = total_without_tax * 0.075
+    total_sum = tax + total_without_tax 
     total_sum.round(2)
     #Initialize variable outside of loop 
     #Iterate through each key(product) + value(price) in the products hash
-    #
-
-
     # calculate the total cost of the order by:
     # Summing up the products
     # Adding a 7.5% tax
@@ -42,11 +37,38 @@ class Order
     @products[name] = price
   end
 
-  #   # if variable.callmethod() != nil 
-  #   #   #Raise Argument Error here
-  #   # else 
-  #   #   @products << add_product
-  #   # end 
-  # end  
+  def self.all
+    all_orders = []
 
+    CSV.read('data/orders.csv').each do |order|
+      id = order[0].to_i
+      products = {}
+      @item_price_array = order[1].split(";")
+
+      #parse through product by item/price
+      @item_price_array.each do |product|
+        split_product = product.split(":")
+        item = split_product[0]
+        price = split_product[1].to_f
+        products_hash = {item => price}
+        products.merge!(products_hash)
+      end
+
+      customer = Customer.find(order[2].to_i)
+      status = order[3].to_sym
+      all_orders << Order.new(id.to_i, products, customer, status)
+
+    end 
+    return all_orders
+  end
+
+
+  def self.find(id)
+    self.all.each do |order|
+      if id == order.id
+        return order
+      end
+    end
+  return nil 
+  end
 end
