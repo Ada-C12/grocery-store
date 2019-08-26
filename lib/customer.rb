6c80a311cc49dc2eedc934da5bc716d3144ad447
m.dove@live.com <m.dove@live.com>
2019-08-25 18:27:48 -0700

    Added comments. Unblocked all tests. All tests passing at this time.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,16 +1,19 @@
 require "csv"
 require "awesome_print"
 
+# Build Customer class, instantiate readers for instance variables.
 class Customer
-  attr_reader :id, :all_customers
-  attr_accessor :email, :address
+  attr_reader :id, :all_customers, :email, :address
 
+  # Build constructor to accept parameters od id, email, and address and
+  #create instance variables.
   def initialize(id, email, address)
     @id = id
     @email = email
     @address = address
   end
 
+  # All method reads every customer from given CSV and returns a formatted array.
   def self.all
     all_customers = []
     array_array_customers = CSV.read("data/customers.csv")
@@ -21,6 +24,8 @@ class Customer
     return all_customers
   end
 
+  # find class method takes in id as an argument and searches
+  # through .all's return to find a matching customer.
   def self.find(id)
     array_customers = Customer.all
     array_customers.each do |customer|
--- lib/order.rb
+++ lib/order.rb
@@ -1,34 +1,39 @@
 require_relative "customer.rb"
 require "csv"
-require "awesome_print"
-require "json"
 
+# Build Order Class and instantiate instance variables include readers for all attributes
 class Order
   attr_reader :id, :products, :customer, :fulfillment_status
   VALID_STATUSES = [:pending, :paid, :processing, :shipped, :complete]
 
+  # Build initialize method to take in parameters with a default fulfillment status.
   def initialize(id, products, customer, fulfillment_status = :pending)
     @id = id
     @products = products
     @customer = customer
 
+    # Validate that input was among the included possible statuses. Raise error, otherwise.
     if !VALID_STATUSES.include?(fulfillment_status)
       raise ArgumentError.new("Invalid Status")
     end
     @fulfillment_status = fulfillment_status
   end
 
+  # total instance method calculates the total cost of any order by summing  products, then adding tax. 
   def total()
     total_amount = 0.0
     @products.each do |product, price|
       if @products.empty?
         total_amount = 0.0
-      else total_amount += price.to_f       end
+      else total_amount += price.to_f      
+     end
     end
     total_amount = (total_amount * 1.075)
     return (sprintf("%.2f", total_amount).to_f)
   end
 
+  # add_product instance method takes in two arguments to add a product into order collection
+  # as long as the product does not already exist in the collection. 
   def add_product(product_name, product_price)
     if @products.has_key?(product_name)
       raise ArgumentError.new("That product already exists in this collection")
@@ -37,6 +42,7 @@ class Order
     end
   end
 
+  # remove_product instance method remoces a product from a collection ny searching for keys matching given input. 
   def remove_product(product_name)
     if !@products.has_key?(product_name)
       raise ArgumentError.new("That product is not in the collection anyway")
@@ -46,6 +52,8 @@ class Order
     return @products
   end
 
+  # all class method reads full collection of orders from the csv.
+  # Formatted by making products in each order into a hash and pushing each attribute into all_orders
   def self.all
     all_orders = []
     split_by_item = []
@@ -69,6 +77,8 @@ class Order
     return all_orders
   end
 
+  # find class method finds and returns an order given its ID as an input.
+  # calls on all method to format in a searchable way. 
   def self.find(order_num)
     orders = self.all
     orders.each do |order|
@@ -80,8 +90,3 @@ class Order
     return nil
   end
 end
-
-ap Order.all.take(10)
-
-# 1,---Lobster:17.18;Annatto seed:58.38;Camomile:83.21,---25, ---complete
-# 2,---Sun dried tomatoes:90.16;Mastic:52.69;Nori:63.09;Cabbage:5.35, ---10, ---paid
